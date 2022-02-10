e3420dc4132fedce5355c9080b931bce5b0a3cf1
shafin_shant0 <shafinshanto9@gmail.com>
2022-02-10 13:16:28 +0600

    complete

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -19,7 +19,10 @@ const Footer = () => (
       </div>
 
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__cormorant" style={{
+           color:"white",
+           marginBottom:'15px'
+        }}>Food Valley</p>
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
         <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
         <div className="app__footer-links_icons">
